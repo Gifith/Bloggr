0b517270c42245e6452cc5063d5abd608d2d6500
Clément Toutain <clementtoutain@gmail.com>
2019-06-11 14:31:31 +0200

    Update tokens.py

--- app/tokens.py
+++ app/tokens.py
@@ -4,4 +4,4 @@ TokensAPI = Blueprint('TokensApi', __name__, url_prefix="/tokens")
 
 @TokensAPI.route("/", methods=["GET"])
 def get_tokens():
-    return "Hello Tokens"
+    return "Hello Token !!"
