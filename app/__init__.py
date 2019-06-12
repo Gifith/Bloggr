84326f92fbfaf6e5fcceddf065bcf0fd7be83fa5
martin.mdhk <martin.mdhk@gmail.com>
2019-06-12 12:01:11 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from .tokens import TokensAPI
 from .posts import PostAPI
 from .app import app
 from db.modele import User
+from db import db
 
 
 
@@ -27,6 +28,7 @@ else:
         remove(join(abspath(dirname(dirname(__file__))), 'storage', 'app.db'))
 
 db.create_all()
+
 user1 = User(id = '01', username = 'premier', email = "premier@st.com", hash = '721GBBH1', sel = 'IJZNhnahzb', role = 0, active = 1)
 user2 = User(id = '02', username = 'second', email = "second@nd.com", hash = 'dzah124NDUI7', sel = 'age2d2R3', role = 0, active = 1)
 user3 = User(id = '03', username = 'third', email = "third@rd.com", hash = 'gazr7nd7', sel = 'az928Klm2', role = 0, active = 1)
