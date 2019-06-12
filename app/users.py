5fac54cfc88eb9fe6f58cd35949824bc394a9597
martin.mdhk <martin.mdhk@gmail.com>
2019-06-12 13:53:16 +0200

    add userform

--- app/users.py
+++ app/users.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from os.path import join, dirname, abspath
 
 UsersAPI = Blueprint('UsersApi', __name__, url_prefix="/users")
 
 @UsersAPI.route("/", methods=["GET"])
 def get_users():
     return "Hello Users, hey ya"
+
+
+@UsersAPI.route("/create", methods=["GET"])
+def get_userform():
+    return render_template('userform.jinja')
+
+
+
--- db/db.py
+++ db/db.py
@@ -3,3 +3,4 @@ from os.path import join, abspath, dirname
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + join(abspath(dirname(dirname(__file__))), 'storage', 'app.db')
 db = SQLAlchemy(app)
+print app.config['SQLALCHEMY_DATABASE_URI']
