84f891032fb678af6883331606f98264c5187c7c
martin.mdhk <martin.mdhk@gmail.com>
2019-06-13 10:12:30 +0200

    Decorators

---
+++ decorators/login.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from flask import g, request, redirect, url_for, abort
+from db import db
+from db.modele import Token
+import datetime
+
+def require_login(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if request.cookies.get("token") is None:
+            abort(401)
+        else:
+            info = Token.query.filter_by(jwt=request.cookies.get("token")).first()
+            if info is None:
+                abort(401)
+            elif  (datetime.datetime.combine(datetime.date(2019,6,5), datetime.datetime.min.time()) - info.expiration).seconds > 7200:
+                abort(401)
+            else:
+                return f(*args, **kwargs)
+    return decorated_function
+
+
+            
