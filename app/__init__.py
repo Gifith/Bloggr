2979f137cb309da133c59f6ebfc7365a18dc43e8
Clément Toutain <clementtoutain@gmail.com>
2019-06-13 11:41:50 +0200

    Update __init__.py
    création post

--- app/__init__.py
+++ app/__init__.py
@@ -41,12 +41,20 @@ storagedir = join(rootdir,'storage')
 if not isfile( join(storagedir, 'app.db') ) and not exists(storagedir):
     makedirs(storagedir)
     db.create_all()
-    user1 = User(id = '01', username = 'premier', email = "premier@st.com", hash = '721GBBH1', sel = 'IJZNhnahzb', role = 0, active = 1)
-    user2 = User(id = '02', username = 'second', email = "second@nd.com", hash = 'dzah124NDUI7', sel = 'age2d2R3', role = 0, active = 1)
-    user3 = User(id = '03', username = 'third', email = "third@rd.com", hash = 'gazr7nd7', sel = 'az928Klm2', role = 0, active = 1)
+    user1 = User(id = 1, username = 'premier', email = "premier@st.com", hash = '721GBBH1', sel = 'IJZNhnahzb', role = 0, active = 1)
+    user2 = User(id = 2, username = 'second', email = "second@nd.com", hash = 'dzah124NDUI7', sel = 'age2d2R3', role = 0, active = 1)
+    user3 = User(id = 3, username = 'third', email = "third@rd.com", hash = 'gazr7nd7', sel = 'az928Klm2', role = 0, active = 1)
     token1 = Token(jwt = 'XXXXXXXXXXXXXXXXXXXXX', expiration = datetime(2022, 12, 14, 10, 0, 0))
     token2 = Token(jwt = 'YYYYYYYYYYYYYYYYYYYYY', expiration = datetime(2002, 3, 15, 10, 0, 0))
     token3 = Token(jwt = 'ZZZZZZZZZZZZZZZZZZZZZ', expiration = datetime(2222, 7, 10, 10, 0, 0))
+    post1 = Post(id = 1, titre = 'Un ours acrobate démoli', corpus = 'Après son spectacle un ours acrobate se casse les 4 pattes en sautant du trapèze',
+        datecree = datetime(2019, 3, 15, 10, 0, 0), datemodif = datetime(2019, 3, 15, 10, 0, 0),creator = 1 , Image = 'https://cap.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcap.2F2018.2F11.2F19.2F152f6726-86b2-460f-8c42-1a679ff88a2e.2Ejpeg/750x375/background-color/ffffff/quality/70/debat-a-t-on-bien-fait-de-reintroduire-lours-dans-les-pyrenees-1316330.jpg' , active = True)
+    post2 = Post(id = 2, titre = 'Plus de pension alimentaire', corpus = 'La pension alimentaire est supprimé en France des le 29 juillet',
+        datecree = datetime(2019, 3, 17, 10, 0, 0), datemodif = datetime(2019, 3, 17, 10, 0, 0),creator = 2, Image = 'https://ichef.bbci.co.uk/news/660/cpsprodpb/174DC/production/_99325459_gettyimages-825838988.jpg' , active = True)
+    post3 = Post(id = 3, titre = 'Concours de Hula-Hoop', corpus = 'Jeannine est la gagnant du concours de Timilly sur Eure',
+        datecree = datetime(2019, 3, 17, 10, 0, 0), datemodif = datetime(2019, 3, 17, 10, 0, 0),creator = 3, Image = 'https://bilder.hula-hoop-shop.eu/media/image/product/6141/md/hula-hoop-pour-les-enfants-colore-diamctre-80-75-70-65-60cm.jpg' , active = True)
+
+
 
     db.session.add(user1)
     db.session.add(user2)
@@ -54,6 +62,9 @@ if not isfile( join(storagedir, 'app.db') ) and not exists(storagedir):
     db.session.add(token1)
     db.session.add(token2)
     db.session.add(token3)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.add(post3)
     db.session.commit()
         
 else:
