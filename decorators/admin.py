834e12c61999b4cb6640a71b70babd5d4c72911c
martin.mdhk <martin.mdhk@gmail.com>
2019-06-14 16:39:44 +0200

    Update

--- decorators/admin.py
+++ decorators/admin.py
@@ -6,7 +6,7 @@ from db.modele import Token
 def require_admin(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None: abort(401)
+        if g.user.username is None: abort(401)
         elif g.role == 1: return f(*args, **kwargs)
         else: abort(403)
     return decorated_function
